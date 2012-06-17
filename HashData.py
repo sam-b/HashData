d49d9cc2719b9e8a46c5103a8b661d9fb846a744
Sam Brown <sam@datacurse.org>
2012-06-17 20:55:02 +0100

    some actual code! :o

---
+++ HashData.py
@@ -0,0 +1,9 @@
+import re
+
+reg1=("[A-Fa-f0-9]{32}","MD2, MD4, MD5, NTLM or RIPEMD-128")
+reg2=("[A-Fa-f0-9]{40}","SHA160Bit(SHA1),SQL 4.x or RIPEMD-160")
+reg3=("[A-Fa-f0-9]{64}","SHA 256Bit")
+reg4=("[A-Fa-f0-9]{98}","SHA 384Bit")
+reg5=("[A-Fa-f0-9]{130}","SHA 512Bit")
+reg6=("[A-Fa-f0-9]{16}","LM, VNC or MySQL323")
+
