3dbe58565d7f76a6e1e0dd2352013d210c7ec635
Sam Brown <sam@datacurse.org>
2012-06-17 21:46:58 +0100

    it works

--- HashData.py
+++ HashData.py
@@ -1,9 +1,14 @@
 import re
-
-reg1=("[A-Fa-f0-9]{32}","MD2, MD4, MD5, NTLM or RIPEMD-128")
-reg2=("[A-Fa-f0-9]{40}","SHA160Bit(SHA1),SQL 4.x or RIPEMD-160")
-reg3=("[A-Fa-f0-9]{64}","SHA 256Bit")
-reg4=("[A-Fa-f0-9]{98}","SHA 384Bit")
-reg5=("[A-Fa-f0-9]{130}","SHA 512Bit")
-reg6=("[A-Fa-f0-9]{16}","LM, VNC or MySQL323")
-
+import sys
+regexs={("[A-Fa-f0-9]{32}","MD2, MD4, MD5, NTLM or RIPEMD-128"),
+("[A-Fa-f0-9]{40}","SHA160Bit(SHA1),SQL 4.x or RIPEMD-160"),
+("[A-Fa-f0-9]{64}","SHA 256Bit(Unix)"),
+("[A-Fa-f0-9]{98}","SHA 384Bit"),
+("[A-Fa-f0-9]{130}","SHA 512Bit(Unix)"),
+("[A-Fa-f0-9]{16}","LM, VNC or MySQL323")}
+if(len(sys.argv)>1):
+	for i in regexs:
+		if(re.match(i[0],sys.argv[1])):
+			print i[1]
+else:
+	print "please provide your hash as a commandline arguement!"
