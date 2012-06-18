19604b0baaecab705bcb519c3e3eeef1ba97eb8f
Sam Brown <samdanielbrown@gmail.com>
2012-06-18 23:04:38 +0200

    Update master

--- HashData.py
+++ HashData.py
@@ -13,9 +13,8 @@ regexs={("^[A-Fa-f0-9]{32}$","MD2, MD4, MD5, NTLM or RIPEMD-128"),
 ("^.{13}$","DES"),
 ("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$","Base64"),
 ("^[0-9a-f]{120}$","Whirlpool, Whirlpoll (HMAC)"),
-("^[0-9a-f]{8}$","ADLER32"),
+("^[0-9a-f]{8}$","ADLER32,CRC32,CRC32B"),
 ("^[0-9a-f]{4}$","CRC16,CRC16CCITT,FCS16"),
-("^[0-9a-f]{8}$","CRC32,CRC32B"),
 ("^[0-9a-f]{32}$","DomainCachedCredentials"),
 ("^[0-9]{8}$","GHash323.GHash325")}
 
