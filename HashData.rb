30dbe2b1537ba16ce43e6a15bc500463720f6d4d
Sam Brown <sam@datacurse.org>
2012-07-08 17:50:30 +0100

    added more hash functions, getting pretty obscure now

--- HashData.rb
+++ HashData.rb
@@ -13,13 +13,13 @@ regexs=[[/^[A-Fa-f0-9]{32}$/,"MD2, MD4, MD5, NTLM or RIPEMD-128"],
 [/^[A-Fa-f0-9]{16}$/,"LM, VNC or MySQL323"],
 [/^.{13}$/,"DES"],
 [/^(?:[A-Za-z0-9\+]{4})*(?:[A-Za-z0-9\+]{2}==|[A-Za-z0-9\+]{3}=|[A-Za-z0-9\+]{4})$/,"Base64"],
-[/^[0-9a-f]{120}$/,"Whirlpool, Whirlpoll [HMAC]"],
+[/^[0-9a-f]{120}$/,"Whirlpool, Whirlpool [HMAC]"],
 [/^[0-9a-f]{8}$/,"ADLER32,CRC32,CRC32B"],
 [/^[0-9a-f]{4}$/,"CRC16,CRC16CCITT,FCS16"],
-[/^[0-9a-f]{32}$/,"DomainCachedCredentials,Haval128,RAdmin v2.x,RipeMD128"],
+[/^[0-9a-f]{32}$/,"DomainCachedCredentials,Haval128,RAdmin v2.x,RipeMD128,SNEFRU128,Tiger128"],
 [/^[0-9]{8}$/,"GHash323.GHash325"],
-[/^[a-f0-9]{64}$/,"GOSTR341194, Haval256,RipeMD256"],
-[/^[a-f0-9]{40}$/,"Haval160,MySQl5,RipeMD160"],
+[/^[a-f0-9]{64}$/,"GOSTR341194, Haval256,RipeMD256,SNEFRU256"],
+[/^[a-f0-9]{40}$/,"Haval160,MySQl5,RipeMD160,Tiger160"],
 [/^[a-f0-9]{48}$/,"Haval192"],
 [/^[a-f0-9]{56}$/,"Haval224"],
 [/^0x[a-f0-9]{30}$/,"LineageIIC4"],
@@ -27,9 +27,15 @@ regexs=[[/^[A-Fa-f0-9]{32}$/,"MD2, MD4, MD5, NTLM or RIPEMD-128"],
 [/^\$H\$.{31}$/,"MD5phpBB3"],
 [/^\$1\$.{31}$/,"MD5Unix"],
 [/^\$P\$.{31}$/,"MD5WordPress"],
-[/^[a-f0-9]{80}$/,"RipeMD320"]]
+[/^[a-f0-9]{80}$/,"RipeMD320"],
+[/^sha1\$[A-Fa-f0-9]{47}$/,"SHA1Django"],
+[/^[a-fA-F0-9]{56}$/,"SHA224"],
+[/^[a-fA-F0-9]{96}$/,"SHA384"],
+[/^[a-f0-9]{47}$/,"Tiger192"],
+[/^[a-f0-9]{8}$/,"XOR-32"],
+[/^\*[a-f0-9]{40}]$/,"MySQl 160bit"]]
 
-puts"
+puts "
 HashData Version 0.2 By DataCurse [Sam Brown, sam@datacurse.org]
 mit license
 Enter exit to exit program.
