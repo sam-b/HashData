17a72a2d16aca130af2f2aaca6046dd5bc888c5d
Sam Brown <sam@datacurse.org>
2012-09-13 16:40:15 +0100

    plausibility checker actually semi useful now

--- TextCheck.rb
+++ TextCheck.rb
@@ -1,19 +1,35 @@
 #shall see whether a string is plausibly english/plaintext or not
 class TextCheck
-	@@never_appears = ['ae','aj','bj','bm','bs','bt','cd','cf','cm','cn','cs','cw','cy','dj','dy','ej',
+	require 'set'
+	@@never_appears = Set.new(['ae','aj','bj','bm','bs','bt','cd','cf','cm','cn','cs','cw','cy','dj','dy','ej',
 						'ez','fd','fg','fm','fp','fw','fy','gd','gg','gj','gm','gw','hb','hl','hp','hq',
 						'hw','hy','ja','ka','kc','kl','kn','ks','lg','lh','ln','md','mf','mh','nj','nq',
 						'oj','ox','pb','pc','pd','pn','pv','pw','py','qm','qr','rj','rk','sk','sv','sy',
 						'tg','tq','tz','uf','uo','uv','vo','vt','wl','wr','ws','wy','xd','xe','xf','xh',
-						'xn','xo','xr','xs','yg','yh','yu','yw','za','zi']
+						'xn','xo','xr','xs','yg','yh','yu','yw','za','zi'])
 	def check(input)
 		viable = []
 		i = 0
-		puts @@never_appears[0]
+		j = 0
 		while i < input.length
-			viable[i] = input[i]
+			if viable(input[i])
+				viable[j] = input[i]
+				j += 1
+			end
 			i +=1
 		end
 		return viable
+	end
+	def viable(input)
+		i = 0
+		j = 1
+		while j < input.length
+			if @@never_appears.include?((input[i] + input[j]))
+				return false
+			end
+			i +=1
+			j +=1
+		end
+		return true
 	end
 end
