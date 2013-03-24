0c7535a539e3dcb8d34ff521c53763cbf4c53059
root <root@glados.(none)>
2013-03-24 17:31:07 +0000

    fixed some regexs and added a load more

--- HashData.rb
+++ HashData.rb
@@ -1,17 +1,19 @@
+#!/usr/bin/ruby
+
 ########################################################################
 # HashData Version 0.2 By Sam Brown, sam@datacurse.org
 # mit license
 ###########################################################################
-#!/usr/bin/ruby
 
 #create a 2d array of regexs and the names of the functions they match
 require './HashChecker.rb'
+
+hash = HashChecker.new
 puts "
-HashData Version 0.2 By DataCurse [Sam Brown, sam@datacurse.org]
+HashData Version 0.3 By Sam Brown, sam@datacurse.org
 mit license
 Enter exit to exit program.
 "
-hash = HashChecker.new
 while true do
 	puts "Please enter your Hash:"
 	input = gets
