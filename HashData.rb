bedeff08b58a0364ad693bc67bb6d648a2fda3c2
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-01-02 22:33:38 +0000

    Refactor and beautify

--- HashData.rb
+++ HashData.rb
@@ -1,29 +1,12 @@
-########################################################################
-# HashData Version 0.2 By Sam Brown, sam@datacurse.org
-# mit license
-###########################################################################
-
-#create a 2d array of regexs and the names of the functions they match
 require './HashChecker.rb'
 
+puts 'Use Ctrl+C to exit program.'
 hash = HashChecker.new
-puts "
-HashData Version 0.3 By Sam Brown, sam@datacurse.org
-mit license
-Enter exit to exit program.
-"
-while true do
-	puts "Please enter your Hash:"
-	input = gets
-	input=input.chop
-	#get the string the user wants to identify end if they type exit
-	if input == "exit" then
-		break
-	end
-	out = hash.check(input)
-	if out != "" then
-		puts out
-	else
-		puts "Sorry - no hash matches found!"
-	end
+
+loop do
+  puts 'Enter your Hash: '
+  trap('SIGINT') { exit }
+  out = hash.check(gets.chomp)
+  out.empty? ? puts('No hash matches found!') : puts(out)
+  puts
 end
